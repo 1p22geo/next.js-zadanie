99d2661ba67798d134917e44f730955ab35c7cbc
1p22geo <1p22geodecki@gmail.com>
2022-12-18 15:40:13 +0100

    JAK TO "this is undefined"

--- pages/api/get.js
+++ pages/api/get.js
@@ -10,6 +10,7 @@ export default async function handler(req, res) {
   const collection = db.collection('test');
 
   const findResult = await collection.find({}).toArray();
-
+  client.close()
+  
   res.status(200).json({ result: findResult })
-  }
+}
--- pages/layout.js
+++ pages/layout.js
@@ -8,7 +8,23 @@ export default class Layout2 extends React.Component{
     this.state = {records:[]}
   }
   render(){
-    return React.createElement(component, this.state);
+    return <div className='p-10' onClick={this.a}>{React.createElement(component, this.state)}</div>//React.createElement(component, this.state);
   }
+  async a(){
+    const response = await fetch("/api/get", {
+      method: "GET"});
+    let json_response = await response.json();
+    let arr = [];
+    for (let n = 0; n < json_response.result.length; n++) {
+      const document = json_response.result[n];
+      arr.push({
+        title:document.name,
+        text:document.age
+      });
+    }
+    this.setState({
+      records:arr
+    });
+    //fetch('/api/get').then(response => response.json()).then(data => console.log(data));
+    }
 }
-
