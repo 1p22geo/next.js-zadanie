e5a9832948f447aaf93499476005ea3ae8e575a7
1p22geo <1p22geodecki@gmail.com>
2022-12-23 20:05:10 +0100

    cinema stuff begins here

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,7 +15,7 @@ const Home: NextPage = () => {
   return (
     <div className='bg-black'>
     <div className="text-6xl flex font-bold bg-[#14213D] p-5 w-full text-[#E5E5E5] pb-12 justify-between border-[#FCA311] border-b-8">
-          <h1>Database connection</h1>
+          <h1>Cinema database</h1>
           <img src='vercel.svg' className='pr-12'/>
     </div>
     
@@ -30,42 +30,7 @@ const Home: NextPage = () => {
       <Login/>
         
 
-        <form className='p-5 mt-10 bg-[#E5E5E5] rounded-xl border-[#FCA311] border-b-8'>
-        <h1 className="text-2xl font-bold mb-3">
-          Search users with age between:
-        </h1>
-          <p className='p-2'>Min: <input className='p-1 bg-slate-400 text-white ml-1 focus:bg-[#FCA311]' type={'text'} id={'min'} name={'min'}/><br/></p>
-          <p className='p-2'>Max: <input className='p-1 bg-slate-400 text-white ml-1 focus:bg-[#FCA311]' type={'text'} id={'max'} name={'max'}/><br/></p>
-        </form>
-
-        <Layout2/>
-        <div className='p-5 mt-10 bg-[#E5E5E5] rounded-xl border-[#FCA311] border-b-8'>
-        <h1 className="text-2xl font-bold mb-3">
-          Add another user:
-        </h1>
-        <form>
-          <p className='p-2'>Name: <input className='p-1 bg-slate-400 text-white ml-1 focus:bg-[#FCA311]' type={'text'} id={'Name'} name={'Name'}/><br/></p>
-          <p className='p-2'>Age: <input className='p-1 bg-slate-400 text-white ml-1 focus:bg-[#FCA311]' type={'text'} id={'Age'} name={'Age'}/><br/></p>
-        </form>
-          <button className='bg-[#FCA311] rounded-md p-1 active:bg-slate-300' onClick={()=>{
-            let nameInput = document.getElementById('Name') as HTMLInputElement;
-            let name;
-            if(nameInput){
-              name = nameInput.value
-            }
-            let ageInput = document.getElementById('Age') as HTMLInputElement;
-            let age;
-            if(ageInput){
-              age = +ageInput.value
-            }
-            console.log(name, age)
-            fetch("/api/db_write", {
-              method: "POST",
-              body:JSON.stringify({name:name, age:age})
-          })
-          }}>Submit data</button>
-          </div>
-          
+                  
           <div className='p-5 mt-10 bg-[#E5E5E5] rounded-xl border-[#FCA311] border-b-8'>
         <h1 className="text-2xl font-bold mb-3">
           Add another user:
